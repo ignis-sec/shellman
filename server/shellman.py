a694e68874e178dffd8e9319b4674bebb4217efe
goeo_ <me@goeo.pink>
2020-04-24 15:30:19 +0200

    added tls cert generation + SSLContext initialization (we can accept connections over tls now!)

--- server/shellman.py
+++ server/shellman.py
@@ -1,5 +1,7 @@
 import socket
 import asyncio
+import ssl
+import os
 
 from singleton import singleton
 from config import Config
@@ -13,19 +15,31 @@ class ShellmanCore:
 
     def __init__(self):
         # set up ssl context
+        self.ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+
+        with open("./cert.pem", "w") as cert:
+            cert.write(Config()['tls']['cert'])
+        with open("./private.key", "w") as cert:
+            cert.write(Config()['tls']['key'])
+
+        self.ssl_ctx.load_cert_chain('./cert.pem', './private.key')
+
+        os.remove('./cert.pem')
+        os.remove('./private.key')
+
         # import modules
 
-        loop = asyncio.new_event_loop()
+        loop = asyncio.get_event_loop()
         asyncio.set_event_loop(loop)
 
         loop.create_task(self.start_listening(Config()['connection']['port']))
-        loop.create_task(self.start_listening(12111))
+        # loop.create_task(self.start_listening(12111))
         loop.run_forever()
         # start checking connections' readers for updates
 
     async def start_listening(self, port):
         server = await asyncio.start_server(self.client_connected, Config()['connection']['host'],
-                                   port, family=socket.AF_INET, ssl=self.ssl_ctx, start_serving=True)
+                                   port, family=socket.AF_INET, ssl=self.ssl_ctx)
         addr = server.sockets[0].getsockname()
         print(f'Serving on {addr}')
 
--- server/wizard.py
+++ server/wizard.py
@@ -1,4 +1,5 @@
 from config import Config
+from OpenSSL import crypto, SSL
 
 
 def shellman_wizard():
@@ -10,3 +11,37 @@ def shellman_wizard():
         'allow_frontend_to_listen': True
     }
     Config().write()
+    print("Generating TLS certificate...")
+    cert_gen()
+    print("Done!")
+
+
+def cert_gen():
+    # create a key pair
+    k = crypto.PKey()
+    k.generate_key(crypto.TYPE_RSA, 4096)
+    # create a self-signed cert
+    cert = crypto.X509()
+    cert.get_subject().C = "KP"
+    cert.get_subject().ST = "Pyongyang"
+    cert.get_subject().L = "Shellman"
+    cert.get_subject().O = "Shellman"
+    cert.get_subject().OU = "Shellman"
+    cert.get_subject().CN = "Shellman"
+    cert.get_subject().emailAddress = "sh@ellm.an"
+    cert.set_serial_number(0)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(10*365*24*60*60)
+    cert.set_issuer(cert.get_subject())
+    cert.set_pubkey(k)
+    cert.sign(k, 'sha512')
+    cert = crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode("utf-8")
+    key = crypto.dump_privatekey(crypto.FILETYPE_PEM, k).decode("utf-8")
+
+    Config()['tls'] = {
+        'cert': cert,
+        'key': key
+    }
+
+    Config().write()
+
